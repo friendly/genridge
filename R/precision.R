bfcc4e7fd4ff6e9f080cfdafa0d71e726f67b104
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-13 15:29:20 +0000

    Added options to precision.*
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genridge/pkg@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/precision.R
+++ R/precision.R
@@ -1,24 +1,28 @@
 ## Measures of precision and bias for ridge regression
+## 
 
 precision <- function(object, ...) {
 	UseMethod("precision")
 }
 
-# TODO:  should we use log.det or det()^{1/p} ??
-precision.ridge <- function(object, ...) {
+# DONE:  allow choice of log.det or det()^{1/p}
+precision.ridge <- function(object, det.fun=c("log","root"), normalize=TRUE, ...) {
 	tr <- function(x) sum(diag(x))
 	maxeig <- function(x) max(eigen(x)$values)
-
+	
 	V <- object$cov
 	p <- ncol(coef(object))
-	ldet <- log(unlist(lapply(V, det)))
+	det.fun <- match.arg(det.fun)
+	ldet <- unlist(lapply(V, det))
+	ldet <- if(det.fun == "log") log(ldet) else ldet^(1/p)
 	trace <- unlist(lapply(V, tr))
 	meig <- unlist(lapply(V, maxeig))	
 	norm <- sqrt(rowMeans(coef(object)^2))
-	data.frame(lambda=object$lambda, df=object$df, log.det=ldet, trace=trace, max.eig=meig, norm.beta=norm)
+	if (normalize) norm <- norm / max(norm)
+	data.frame(lambda=object$lambda, df=object$df, det=ldet, trace=trace, max.eig=meig, norm.beta=norm)
 }
 
-precision.lm <- function(object, ...) {
+precision.lm <- function(object, det.fun=c("log","root"), normalize=TRUE, ...) {
 	V <- vcov(object)
 	beta <- coefficients(object)
 	if (names(beta[1]) == "(Intercept)") {
@@ -26,10 +30,14 @@ precision.lm <- function(object, ...) {
 		beta <- beta[-1]
 	}
 #	else warning("No intercept: precision may not be sensible.")
-	ldet <- log(det(V))
+	p <- length(beta)
+	det.fun <- match.arg(det.fun)
+	ldet <- det(V)
+	ldet <- if(det.fun == "log") log(ldet) else ldet^(1/p)
 	trace <- sum(diag(V))
 	meig <- max(eigen(V)$values)
 	norm <- sqrt(mean(beta^2))
-	res <- list(df=length(beta), log.det=ldet, trace=trace, max.eig=meig, norm.beta=norm)
+	if (normalize) norm <- norm / max(norm)
+	res <- list(df=length(beta), det=ldet, trace=trace, max.eig=meig, norm.beta=norm)
 	unlist(res)
 }
