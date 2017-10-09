1edad5dab60840eb4d4a910b9cc5edc531216cfe
Michael Friendly <friendly@yorku.ca>
2017-10-08 21:48:48 -0400

    minor code styling

--- R/biplot.pcaridge.R
+++ R/biplot.pcaridge.R
@@ -9,8 +9,8 @@ biplot.ridge <-
 		function(x, variables=1:2, xlab, ylab, ...) {
 	x$svd.V <- t(x$svd.V)
 	vnames <- colnames(coef(x))[variables]
-	if(missing(xlab)) xlab=vnames[1]
-	if(missing(ylab)) ylab=vnames[2]
+	if(missing(xlab)) xlab <- vnames[1]
+	if(missing(ylab)) ylab <- vnames[2]
 	
 	biplot.pcaridge(x, variables, xlab=xlab, ylab=ylab, ...)
 }
@@ -25,18 +25,19 @@ biplot.pcaridge <- function(x, variables=(p-1):p, labels=NULL, asp=1,
 		...) {
 	
 	# more convenient versions of arrows() and text()
-	Arrows <- function(xy, lenxy, length, angle, col, lwd=1) {
-		arrows(xy[1], xy[2], xy[1]+lenxy[,1], xy[2]+lenxy[,2], length=length, angle=angle, lwd=lwd, col=col)
-	}
-	Text <- function(xy, lenxy, text, col="black", cex=1) {
-		text(xy[1]+lenxy[,1], xy[2]+lenxy[,2], text, col=col, cex=cex)
-	}
-	
-	coef <- coef(x)
-	p <- ncol(coef)
-	if(is.null(x$svd.V)) stop("x must have an svd.V component")
-	V <- x$svd.V[,variables]
-	
+  Arrows <- function(xy, lenxy, length, angle, col, lwd=1) {
+    arrows(xy[1], xy[2], xy[1] + lenxy[, 1], xy[2] + lenxy[, 2], 
+           length = length, angle = angle, lwd = lwd, col = col)
+  }
+  Text <- function(xy, lenxy, text, col="black", cex=1) {
+    text(xy[1] + lenxy[, 1], xy[2] + lenxy[, 2], text, col = col, cex = cex)
+  }	
+
+  coef <- coef(x)
+  p <- ncol(coef)
+  if (is.null(x$svd.V)) stop("x must have an svd.V component")
+  V <- x$svd.V[, variables]	
+
 	# add
 	pct <- 100*x$svd.D^2 /(sum(x$svd.D^2))
 	if (is.logical(suffix) & suffix)
